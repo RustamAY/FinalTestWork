4adef3862ed5c521dd07b4d78e0b6fc697705f8b
rusyun8 <rustam.yunusov@attolabs.de>
2023-02-24 14:30:24 +0500

    update the  program

--- Program/Program.cs
+++ Program/Program.cs
@@ -25,3 +25,28 @@ void PrintArray(string[] array)
   Console.Write("]");
 }
 
+void ChangeArray(string[] array)
+{
+  string[] result = new string[array.Length];
+  Console.Write("[");
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      result[i] = array[i];
+      Console.Write($"\"{array[i]}\"");
+      if (i < array.Length - 1) Console.Write(", ");
+    }
+
+  }
+  Console.Write("]");
+}
+
+Console.WriteLine("Введите длину массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[m];
+
+FillArray(array);
+PrintArray(array);
+Console.Write(" -> ");
+ChangeArray(array);
