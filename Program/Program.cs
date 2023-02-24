955666379abaf61f7a1257747c604274a143797d
rusyun8 <rustam.yunusov@attolabs.de>
2023-02-24 14:03:17 +0500

    add metods for filling and printing array

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача:
+
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решение не рекомендуется
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+void FillArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.WriteLine("Введите значение массива: ");
+    array[i] = Console.ReadLine();
+  }
+}
+
+void PrintArray(string[] array)
+{
+  Console.Write("[");
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"\"{array[i]}\"");
+    if (i < array.Length - 1) Console.Write(", ");
+  }
+  Console.Write("]");
+}
+
